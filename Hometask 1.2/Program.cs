f2b0ef03b004e8893697771ca235371145425985
Jahangirov97 <xaliq1919@gmail.com>
2023-03-05 14:16:00 +0400

    Second commit.

---
+++ Hometask 1.2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Hometask_1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Budget of Azerbaijan Republic
+            double budget = 20000000000;
+            int defencePercentOfBudget = 20;
+            int highTechnologyPercentOfBudget = 10;
+
+            Console.WriteLine("Büdcə :");
+            Console.WriteLine(budget);
+
+            Console.WriteLine("Müdafiə Nazirliyinə :");
+  
+            double defenceBudget = budget * defencePercentOfBudget / 100;
+
+            Console.WriteLine(defenceBudget);
+
+
+            Console.WriteLine("Yüksək Texnologiyalar Nazirliyinə :");
+            double highTechnologyfBudget = budget * highTechnologyPercentOfBudget / 100;
+            Console.WriteLine(highTechnologyfBudget);
+
+
+            Console.WriteLine("Yerdə' qalan büdcə :");
+            double restBudget = budget - defenceBudget - highTechnologyfBudget;
+
+            Console.WriteLine(restBudget );
+
+        }
+    }
+}
